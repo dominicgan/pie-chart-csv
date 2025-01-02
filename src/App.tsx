5505e196c091095c4aad50a1e631dd55ce5b210f
Dominic Gan <dominic.gan@accenture.com>
2025-01-02 11:53:35 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import csv from "csvtojson";
+import PieChartComponent, {
+  PieChartComponentInterface,
+} from "./components/PieChartComponent";
+
+function isString(entry: FormDataEntryValue): entry is string {
+  return typeof entry === "string";
+}
+
+function App() {
+  const [pieData, setPieData] = useState<PieChartComponentInterface["data"]>(
+    []
+  );
+  const [errorMsg, setErrorMsg] = useState<null | string>(null);
+  const [segmentCount, setSegmentCount] = useState(6);
+
+  const formRef = useRef<HTMLFormElement>();
+
+  const generateChart = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    // clear errors on retry
+    setErrorMsg(null);
+
+    /**
+     * sample data
+     * 
+     * ```csv
+       "chrome","99"
+       "firefox","25"
+       "safari","32"
+       "edge","15"
+       "huawei","1"
+       "GSA browser","1"
+       "avast","1"
+       "safari (ios)","79"
+    * ```
+     */
+    try {
+      if (!formRef.current) {
+        console.log("form ref is null", null);
+      }
+      const formData = new FormData(formRef.current);
+
+      const csvDataRaw = formData.get("csvData");
+      const segmentValue = formData.get("segmentCount");
+
+      if (segmentValue && isString(segmentValue)) {
+        setSegmentCount(parseInt(segmentValue, 10));
+      }
+
+      window.console.log({ csvDataRaw });
+
+      if (!csvDataRaw) {
+        setErrorMsg("Please fill in some data");
+        return;
+      }
+
+      if (isString(csvDataRaw)) {
+        csv({ noheader: true })
+          .fromString(csvDataRaw)
+          .then((jsonObj) => {
+            console.log(jsonObj);
+            setPieData(jsonObj);
+          });
+      } else {
+        setErrorMsg("Please enter a valid CSV value");
+      }
+    } catch (e) {
+      setErrorMsg((e as Error).message);
+    }
+  };
+  return (
+    <>
+    <h1>CSV to pie chart</h1>
+    <p>Convert a CSV string to a pie chart.</p>
+      <div className="container">
+        <div className="inputBox">
+          <form action="javascript:void(0)" ref={formRef}>
+            <fieldset className="form-fieldset">
+              <label>
+                <span>Chart CSV data</span>
+                <textarea name="csvData" rows={15} />
+              </label>
+              <label>
+                <span>Chart segments to show</span>
+                <input
+                  type="number"
+                  name="segmentCount"
+                  defaultValue={segmentCount}
+                />
+              </label>
+            </fieldset>
+          </form>
+          {errorMsg && <div className="errorContainer">{errorMsg}</div>}
+
+          <button className="submitBtn" type="button" onClick={generateChart}>
+            Generate chart
+          </button>
+
+          {pieData.length > 0 && <PieChartComponent segments={segmentCount} data={pieData} />}
+          {/* <pre>{JSON.stringify(pieData, null, 4)}</pre> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PieChartComponent.tsx
@@ -0,0 +1,87 @@
+import React, { FC } from "react";
+import {
+  VictoryChart,
+  VictoryContainer,
+  VictoryLegend,
+  VictoryPie,
+  VictoryTheme,
+} from "victory";
+
+export interface PieChartComponentInterface {
+  segments: number;
+  data: Array<{
+    field1: string;
+    field2: string;
+  }>;
+}
+
+const convertTwoDigitDecimal = (value: number): number =>
+  Math.floor(value * 100) / 100;
+
+const PieChartComponent: FC<PieChartComponentInterface> = ({
+  data,
+  segments,
+}) => {
+  window.console.log({ segments });
+
+  const formattedData = data
+    .map(({ field1, field2 }) => ({
+      key: field1,
+      value: parseInt(field2, 10),
+    }))
+    .sort((a, b) => b.value - a.value);
+  const total = formattedData.reduce((acc, { value }) => acc + value, 0);
+
+  const chartData = formattedData.map(({ key, value }) => ({
+    x: `${key} ${convertTwoDigitDecimal((value / total) * 100)}%`,
+    y: value,
+  }));
+
+  const othersData = chartData
+    .slice(segments)
+    .reduce((acc, { y }) => acc + y, 0);
+  const simplifiedChartData =
+    chartData.length > segments
+      ? [
+          ...chartData.slice(0, segments),
+          {
+            x: `Others ${convertTwoDigitDecimal((othersData / total) * 100)}%`,
+            y: othersData,
+          },
+        ]
+      : chartData;
+
+  const legendData = chartData.map(({ x }) => ({
+    name: x,
+  }));
+
+  return (
+    <div className="chart">
+      <div className="pie">
+        <VictoryPie
+          containerComponent={<VictoryContainer responsive={true} />}
+          data={simplifiedChartData}
+          radius={100}
+          labelPlacement="vertical"
+          labelIndicator
+          theme={VictoryTheme.clean}
+        />
+      </div>
+      <VictoryLegend
+        width={900}
+        height={500}
+        itemsPerRow={3}
+        containerComponent={<VictoryContainer responsive={false} />}
+        data={legendData}
+        orientation="horizontal"
+        x={0}
+        y={0}
+        borderPadding={12}
+        gutter={12}
+        theme={VictoryTheme.clean}
+      />
+    </div>
+  );
+};
+
+export default PieChartComponent;
